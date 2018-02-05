27de56ef0a91476cbc74bfa5d7522f8fb6a18de9
Nicholas Hein <nichh81999@gmail.com>
2018-02-04 23:42:35 -0600

    Project Created

---
+++ ActiveModuleManager/main.c
@@ -0,0 +1,19 @@
+/*
+ * ActiveModuleManager.c
+ *
+ * Created: 02/04/2018 11:32:21 PM
+ * Author : Nicholas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+		//...
+		while(1);
+    }
+}
+
